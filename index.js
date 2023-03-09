179e2542b7b169a1ff94e3352f52283731d36bf4
Agrima <er.agrima.malhotra@gmail.com>
2023-03-09 23:28:05 +0530

    feat: add cors middleware

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app=express();
 const router = require('./src/routes');
 require('dotenv').config();
+const cors = require('cors');
 
 app.use(express.json());
 app.use('/',router);
-
+app.use(cors());
 
 const port= process.env.PORT || 4000;
 
